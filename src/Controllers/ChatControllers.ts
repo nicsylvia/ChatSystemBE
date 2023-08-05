4b6d4093001b89f1644d81943ecf15bbc4a14aa3
nicsylvia <nicsylvia15f@gmail.com>
2023-08-05 15:34:33 -0700

    working on cha t 2

--- src/Controllers/ChatControllers.ts
+++ src/Controllers/ChatControllers.ts
@@ -1,5 +1,8 @@
 import ChatModels from "../models/ChatModels";
 
+import { Request, Response } from "express";
+import UserModels from "../models/Usermodels";
+
 export const CreateChat = async (req: Request, res: Response) => {
   try {
     const { userID, friendID } = req.params;
@@ -7,9 +10,22 @@ export const CreateChat = async (req: Request, res: Response) => {
     const friend: any = await UserModels.findById(friendID);
     const user: any = await UserModels.findById(userID);
 
-    if (friend && user) {
-      const chat = await ChatModels.create;
+    const CheckUser = user.friends.some((el) => el === friendID);
+    const CheckFriend = friend.friends.some((el) => el === userID);
+
+    if (CheckFriend && CheckUser) {
+      const chat = await ChatModels.create({
+        members: [userID, friendID],
+      });
+
+      return res.status(400).json({
+        message: "Chats established between Friends",
+        data: chat,
+      });
     } else {
+      return res.status(400).json({
+        message: "Cannot establish chats",
+      });
     }
   } catch (error) {
     return res.status(400).json({
